3a071c6d5c9ca8d7954714b37df53c66811a0366
Unknown <Unknown@DESKTOP-4KSK0J5.fritz.box>
2022-11-15 11:34:08 +0100

    initial commit

--- src/de/demmer/dennis/Application.java
+++ src/de/demmer/dennis/Application.java
@@ -4,7 +4,61 @@ public class Application {
 	
 	
 	public static void main(String[] args) {
+		//Alt + Shift + R
+//		checkEvenWhile(5);
+//		checkEvenFor(1000);
+		
+		//   0   1   2   3   4
+		// | 5 | 4 | 7 | 1 | 9 |
+		
+		int[] intArray = {5 , 4 , 7 , 1 , 9};
+		System.out.println(intArray[2]);
+		
+		
+		int[] intArray2 = new int[5];
+		System.out.println(intArray2[2]);
+		
+		
+		
+		for(int i = 0; i < intArray.length ; i++) {
+			System.out.print(intArray[i]);
+		}
+		
+		
+		
+		
 		
 	}
+
+	
+	
+	static void checkEvenWhile(int length) {
+		
+		int counter = 0;
+		while(counter <= length) {
+				
+			if(counter % 2 == 0) {
+				System.out.println(counter);
+			}
+			
+			counter = counter + 1;
+		}
+	}
+	
+	
+	
+	
+	static void checkEvenFor(int length) {
+
+		// | Zählervariable       |   Laufzeitbedingung    |      Veränderung des Counters |
+		for(int counter = 0       ;   counter <= length    ;      counter = counter + 1    ) {
+			
+			if(counter % 2 == 0) {
+				System.out.println(counter);
+			}
+			
+		}
+	}
+	
 
 }
